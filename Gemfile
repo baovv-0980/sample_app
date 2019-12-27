4235ee563d7e7b0cec47af55e99f362fd8b9042f
vanvietbao <vanvietbao001@gmail.com>
2019-12-27 09:30:54 +0700

    Chapter6: Modeling users

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github){|repo| "https://github.com/#{repo}.git"}
 
 ruby "2.6.3"
 
+gem "bcrypt", "3.1.12"
 gem "bootsnap", ">= 1.4.2", require: false
 gem "bootstrap", "~> 4.0.0"
 gem "config"
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
     get "/home", to: "static_pages#home"
     get "/help", to: "static_pages#help"
     get "/about", to: "static_pages#about"
-    get "/signup", to: "static_pages#signup"
+    get "/signup", to: "users#new"
   end
 end
