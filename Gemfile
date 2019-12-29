2bf917bc4d95ce14b74a762211f5046deaa2daed
vanvietbao <vanvietbao001@gmail.com>
2019-12-29 14:21:19 +0700

    Chapter9: Advanced login

--- Gemfile
+++ Gemfile
@@ -15,8 +15,7 @@ gem "sqlite3", "~> 1.4"
 gem "turbolinks", "~> 5"
 gem "webpacker", "~> 4.0"
 
-gem "tzinfo-data", platforms: %i(mingw mswin x64_mingw jruby)
-
+gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 group :development, :test do
   gem "byebug", platforms: %i(mri mingw x64_mingw)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   VALID_EMAIL_REGEX = Settings.regex_valid_email
+
   before_save{email.downcase!}
   validates :name, presence: true,
                    length: {maximum: Settings.users.your_name.maximum}
@@ -10,4 +13,31 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true,
                        length: {minimum: Settings.users.password.minimum}
+
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
