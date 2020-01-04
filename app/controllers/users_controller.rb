aae52800c3ef427d9cf3309ecdd5a87c39e3a688
vanvietbao <vanvietbao001@gmail.com>
2020-01-04 09:23:27 +0700

    chapter13: User microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
@@ -10,6 +10,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by id: params[:id]
     redirect_to root_url if @user.blank?
+    @microposts = @user.microposts.paginate(page: params[:page],
+                                            per_page: Settings.per_page)
+
   end
 
   def new
@@ -54,16 +57,12 @@ class UsersController < ApplicationController
                                  :password_confirmation
   end
 
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = t ".login_user"
-      redirect_to login_url
-    end
-  end
-
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
-   end
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
